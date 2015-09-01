38ba36c9449502f350185c2a2d5483970744dacf
Michael Topping <mtopping@astro.ucla.edu>
2015-09-01 15:16:31 -0700

    Changed style to match index

--- expand.js
+++ expand.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     //When the toggle div is clicked
     $(".expand-toggle").click(function() {
-        $(this).next(".expand-content").slideToggle();;
+        $(this).next(".expand-content").slideToggle(300, "swing");;
     });
 
 
