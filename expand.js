0f368a7018cf018cfbeed6850a78abd5e12b1b9c
Michael Topping <mtopping@astro.ucla.edu>
2015-09-02 14:37:23 -0700

    Added more content

--- expand.js
+++ expand.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     //When the toggle div is clicked
     $(".expand-toggle").click(function() {
-        $(this).next(".expand-content").slideToggle(300, "swing");;
+        $(this).next(".expand-content").slideToggle(300, "swing");
+        $(this).toggleClass("round-bottom");
     });
 
 
